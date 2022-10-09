fd737e56d9d8fe6efdeb8a025f98bc0304c6a5b1
Guo Y.K <hi@guoyk.net>
2022-10-09 16:25:27 +0800

    add debug.go

---
+++ debug.go
@@ -0,0 +1,74 @@
+package gg
+
+import (
+	"os"
+	"strings"
+)
+
+const (
+	DebugNone      = "-"
+	DebugWildcard  = "*"
+	DebugSeparator = ":"
+)
+
+type DebugSet interface {
+	Debug(v string) bool
+}
+
+type debugSet struct {
+	none  bool
+	items map[string]struct{}
+}
+
+// Debug check weather a debug flag is enabled
+func (d *debugSet) Debug(v string) bool {
+	if d.none {
+		return false
+	}
+
+	if _, ok := d.items[v]; ok {
+		return true
+	}
+
+	for {
+		if _, ok := d.items[v+DebugSeparator+DebugWildcard]; ok {
+			return true
+		}
+
+		if idx := strings.LastIndex(v, DebugSeparator); idx < 0 {
+			break
+		} else {
+			v = v[:idx]
+		}
+	}
+
+	return false
+}
+
+// NewDebugSet create a new DebugSet
+// use comma ',' separate multiple items
+// use '*' for wildcard, for example '*', 'echo:*,view:*'
+func NewDebugSet(debug string) DebugSet {
+	debug = strings.ToLower(strings.TrimSpace(debug))
+	if debug == "" || debug == DebugNone {
+		return &debugSet{none: true}
+	}
+	items := map[string]struct{}{}
+	for _, item := range strings.Split(debug, ",") {
+		item = strings.TrimSpace(item)
+		if item == "" {
+			continue
+		}
+		items[item] = struct{}{}
+	}
+	return &debugSet{items: items}
+}
+
+var (
+	debug = NewDebugSet(os.Getenv("DEBUG"))
+)
+
+// Debug check weather a debug flag is enabled from environment variable $DEBUG
+func Debug(v string) bool {
+	return debug.Debug(v)
+}
---
+++ debug_test.go
@@ -0,0 +1,21 @@
+package gg
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestDebugSet_Debug(t *testing.T) {
+	ds := NewDebugSet("")
+	require.False(t, ds.Debug("aaa"))
+	require.False(t, ds.Debug("bbb"))
+
+	ds = NewDebugSet("echo:*,view:*,,ccc, ,")
+	require.False(t, ds.Debug("aaa"))
+	require.False(t, ds.Debug("bbb"))
+	require.True(t, ds.Debug("echo:aa"))
+	require.True(t, ds.Debug("echo"))
+	require.True(t, ds.Debug("view:aa"))
+	require.True(t, ds.Debug("view"))
+	require.True(t, ds.Debug("ccc"))
+}

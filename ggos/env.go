1acfcd72ec93fd9635181d431d1079ccfbdb75ab
Guo Y.K <hi@guoyk.net>
2022-10-19 10:08:15 +0800

    add ggos.Env()

--- ggos/env.go
+++ ggos/env.go
@@ -7,22 +7,27 @@ import (
 	"strings"
 )
 
+// Env get environment variable from key
+func Env(key string, out *string) {
+	if val := strings.TrimSpace(os.Getenv(key)); val != "" {
+		*out = val
+	}
+}
+
 // MustEnv get environment variable from key, if both value and *out is empty, panic
 func MustEnv(key string, out *string) {
-	val := strings.TrimSpace(os.Getenv(key))
-	if val == "" {
+	if val := strings.TrimSpace(os.Getenv(key)); val != "" {
+		*out = val
+	} else {
 		if *out == "" {
 			panic(errors.New("missing environment variable '" + key + "'"))
 		}
-	} else {
-		*out = val
 	}
 }
 
 // BoolEnv get bool environment variable from key
 func BoolEnv(key string, out *bool) {
-	val := strings.TrimSpace(os.Getenv(key))
-	if val != "" {
+	if val := strings.TrimSpace(os.Getenv(key)); val != "" {
 		*out, _ = strconv.ParseBool(val)
 	}
 }
